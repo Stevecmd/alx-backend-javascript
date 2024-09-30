035918808697130dd4f407584dc8e95ed4af83c8
SteveCmd <murimi101@gmail.com>
2024-09-30 16:11:16 +0300

    Fix

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -3,11 +3,9 @@ export default function getSanFranciscoDescription() {
   const budget = {
     income: '$119,868',
     gdp: '$154.2 billion',
-    gdpPerCapita: '$178,479',
+    capita: '$178,479',
   };
-
-  return `As of ${year}, it was the seventh-highest income county in the United States,`
-    + ` with a per capita personal income of ${budget.income}. As of 2015, San Francisco`
-    + ` proper had a GDP of ${budget.gdp},`
-    + ` and a GDP per capita of ${budget.gdpPerCapita}.`;
+  return `As of ${year}, it was the seventh-highest income county in the United States`
+      + `, with a per capita personal income of ${budget.income}. As of 2015, San Francisco`
+      + ` proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
