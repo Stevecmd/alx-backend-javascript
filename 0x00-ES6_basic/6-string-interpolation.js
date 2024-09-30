427f73482678d155576fdd5f4fc12ce571f2fb08
SteveCmd <murimi101@gmail.com>
2024-09-30 16:06:30 +0300

    missing semi colon fixed

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -4,7 +4,7 @@ export default function getSanFranciscoDescription() {
     income: '$119,868',
     gdp: '$154.2 billion',
     gdpPerCapita: '$178,479',
-  }
+  };
 
   return `As of ${year}, it was the seventh-highest income county in the United States,`
     + ` with a per capita personal income of ${budget.income}. As of 2015, San Francisco`
