7fb5316896616c8060eae74c61217851d60fec3e
SteveCmd <murimi101@gmail.com>
2024-09-30 15:56:21 +0300

    Refactor string interpolation in getSanFranciscoDescription function

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -6,5 +6,8 @@ export default function getSanFranciscoDescription() {
   const gdp = 154.2;
   const gdpPerCapita = 178479;
 
-  return `As of ${year1}, it was the ${incomeRank} income county in the United States, with a per capita personal income of $${income.toLocaleString()}. As of ${year2}, San Francisco proper had a GDP of $${gdp} billion, and a GDP per capita of $${gdpPerCapita.toLocaleString()}.`;
+  return `As of ${year1}, it was the ${incomeRank} income county in the United States,`
+    + `with a per capita personal income of $${income.toLocaleString()}. As of ${year2},`
+    + `San Francisco proper had a GDP of $${gdp} billion,`
+    + `and a GDP per capita of $${gdpPerCapita.toLocaleString()}.`;
 }
