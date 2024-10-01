6f9eba0302a075511b307780cbbb7b0988c65179
SteveCmd <murimi101@gmail.com>
2024-10-01 13:12:33 +0300

    Refactor variable declaration in taskBlock function

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,26 +1,15 @@
-// export function taskFirst() {
-//   const task = 'I prefer const when I can.';
-//   return task;
-// }
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
 
-// export function getLast() {
-//   return ' is okay';
-// }
+export function getLast() {
+  return ' is okay';
+}
 
-// export function taskNext() {
-//   let combination = 'But sometimes let';
-//   combination += getLast();
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-//   return combination;
-// }
-
-export default function taskBlock(trueOrFalse) {
-  let task = false, task2 = true;
-
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
-
-  return [task, task2];
+  return combination;
 }
