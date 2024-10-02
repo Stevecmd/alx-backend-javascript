44e9145c617f2699b251ab71d0d7997081d2bcd5
SteveCmd <murimi101@gmail.com>
2024-10-02 11:20:41 +0300

    Converted the indentation to spaces

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -2,6 +2,6 @@ export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw new Error('cannot divide by 0'); // Throw an error for division by zero
   }
-  return numerator / denominator; // Return the result of the division
+  return numerator / denominator; // Return the result of division
 }
 
