b73febf136eb78c901fb7404a122bfe28962ad43
SteveCmd <murimi101@gmail.com>
2024-10-03 16:01:53 +0300

    Refactor SkyHighBuilding constructor to correctly pass sqft to parent class and use setter for floors validation

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,36 +1,19 @@
 import Building from './5-building';
 
-/**
- * Class representing a sky-high building.
- * @extends Building
- */
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super().sqft = sqft;
     this.floors = floors;
   }
 
-  /**
-   * Returns an evacuation warning message.
-   * @return {string} The evacuation warning message.
-   */
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors.`;
   }
 
-  /**
-   * Gets the number of floors in the building.
-   * @return {number} The number of floors.
-   */
   get floors() {
     return this._floors;
   }
 
-  /**
-   * Sets the number of floors in the building.
-   * @param {number} value - The new number of floors.
-   * @throws {TypeError} If the value is not a number.
-   */
   set floors(value) {
     if (typeof value === 'number') {
       this._floors = value;
