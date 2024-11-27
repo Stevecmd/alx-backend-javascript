efd88ea1367867e9e4eb96f5bc908c1f165fd9cd
SteveCmd <murimi101@gmail.com>
2024-11-27 20:44:09 +0300

    Fix assertion syntax in calculateNumber tests to use .to.equal()

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,43 +5,43 @@ const calculateNumber = require('./2-calcul_chai');
 describe('calculateNumber', () => {
   describe('SUM', () => {
     it('should return 6 when inputs are 1.4 and 4.5', () => {
-      expect(calculateNumber('SUM', 1.4, 4.5), 6);
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
 
     it('should return 0 when inputs are 0.1 and 0.3', () => {
-      expect(calculateNumber('SUM', 0.1, 0.3), 0);
+      expect(calculateNumber('SUM', 0.1, 0.3)).to.equal(0);
     });
 
     it('should return -1 when inputs are -1.4 and -0.5', () => {
-      expect(calculateNumber('SUM', -1.4, -0.5), -1);
+      expect(calculateNumber('SUM', -1.4, -0.5)).to.equal(-1);
     });
   });
 
   describe('SUBTRACT', () => {
     it('should return -4 when inputs are 1.4 and 4.5', () => {
-      expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
 
     it('should return 0 when inputs are 0.1 and 0.3', () => {
-      expect(calculateNumber('SUBTRACT', 0.1, 0.3), 0);
+      expect(calculateNumber('SUBTRACT', 0.1, 0.3)).to.equal(0);
     });
 
     it('should return -1 when inputs are -1.4 and -0.5', () => {
-      expect(calculateNumber('SUBTRACT', -1.4, -0.5), -1);
+      expect(calculateNumber('SUBTRACT', -1.4, -0.5)).to.equal(-1);
     });
   });
 
   describe('DIVIDE', () => {
     it('should return 0.2 when inputs are 1.4 and 4.5', () => {
-      expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
 
     it('should return "Error" when inputs are 1.4 and 0', () => {
-      expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
 
-    it('should return Error when inputs are -1.4 and -0.5', () => {
-      expect(calculateNumber('DIVIDE', 1.4, -0.5), 'Error');
+    it('should return "Error" when inputs are -1.4 and -0.5', () => {
+      expect(calculateNumber('DIVIDE', -1.4, -0.5)).to.equal('Error');
     });
   });
 });
