7eb873159e59f029e60ba1e8da112c033b9f9293
SteveCmd <murimi101@gmail.com>
2024-11-27 08:33:38 +0300

    Change readDatabase function to default export for improved module usage

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -7,7 +7,7 @@ import fs from 'fs';
  * @returns {Promise<object>} - a promise that resolves to an object
  * where each key is a field name and each value is an array of student IDs
  */
-export function readDatabase (filePath) {
+export default function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
