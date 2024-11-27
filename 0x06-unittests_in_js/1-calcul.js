ee1f1a7ae233bdd740045c5bdb5dcb8fdaf6f232
SteveCmd <murimi101@gmail.com>
2024-11-27 13:07:12 +0300

    Refactor calculateNumber function and update test file for consistency

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,4 +1,4 @@
-function calculateNumber(type, a, b) {
+function calculateNumber (type, a, b) {
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
 
@@ -20,7 +20,7 @@ module.exports = calculateNumber;
 
 // If the script is run directly from the command line
 if (require.main === module) {
-    const [,, type, a, b] = process.argv;
-    const result = calculateNumber(type, parseFloat(a), parseFloat(b));
-    console.log(result);
-  }
+  const [,, type, a, b] = process.argv;
+  const result = calculateNumber(type, parseFloat(a), parseFloat(b));
+  console.log(result);
+}
