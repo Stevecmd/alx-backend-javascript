fefd7bad18d00c105f35004c2dc04079e38e1e32
SteveCmd <murimi101@gmail.com>
2024-11-28 12:38:59 +0300

    6. Async tests with done: Add getPaymentTokenFromAPI function and corresponding tests

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+/* eslint-env mocha */
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', function () {
+  it('should return a resolved promise with the correct data when success is true', function (done) {
+    getPaymentTokenFromAPI(true).then(response => {
+      expect(response).to.deep.equal({ data: 'Successful response from the API' });
+      done();
+    }).catch(done);
+  });
+});
