89281828941df1ed9e4f55e77f087738518cfd81
SteveCmd <murimi101@gmail.com>
2024-11-28 13:35:14 +0300

    9. Regex integration testing: Implement basic API with Express and add tests for index and cart pages

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,52 @@
+const request = require('request');
+const { describe, it } = require('mocha');
+const expect = require('chai').expect;
+
+describe('Index page', () => {
+  const url = 'http://localhost:7865/';
+
+  it('should return status code 200', (done) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  const baseUrl = 'http://localhost:7865/cart/';
+
+  it('should return status code 200 when :id is a number', (done) => {
+    request(`${baseUrl}12`, (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is NOT a number', (done) => {
+    request(`${baseUrl}hello`, (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
